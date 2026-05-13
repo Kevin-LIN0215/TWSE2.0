d56a20342f58dd992bb957b27688aec19350364b
Kevin-LIN0215 <a0909132300@gmail.com>
2026-05-13 20:45:18 +0800

    Create stocks.js

---
+++ pages/api/stocks.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://openapi.twse.com.tw/v1/exchangeReport/BWIBBU_ALL"
+    );
+
+    const data = await response.json();
+
+    const stocks = data.map((item) => ({
+      code: item.Code || item.證券代號 || "",
+      name: item.Name || item.證券名稱 || "",
+      pe: Number(item.PEratio || item.本益比 || 0),
+      pb: Number(item.PBratio || item.股價淨值比 || 0),
+      dividendYield: Number(item.DividendYield || item.殖利率 || 0),
+
+      roe: 0,
+      epsGrowth: 0,
+      revenueGrowth: 0,
+      kdSignal: "all",
+      maSignal: "all"
+    }));
+
+    res.status(200).json(stocks);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+}
